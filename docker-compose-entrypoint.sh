3b94d2234b18b4aeb823c026df8fadf2d18cc3da
TheSpad <git@spad.co.uk>
2023-02-16 09:18:03 +0000

    Deprecate-v2

--- docker-compose-entrypoint.sh
+++ docker-compose-entrypoint.sh
@@ -1,6 +1,25 @@
 #!/bin/sh
 set -e
 
+echo '
+┌────────────────────────────────────────────────────┐
+│                                                    │
+│                                                    │
+│             This image is deprecated.              │
+│      We will not offer support for this image      │
+│             and it will not be updated.            │
+│                                                    │
+│                                                    │
+└────────────────────────────────────────────────────┘
+
+Docker Compose is now available from the docker repos:
+https://docs.docker.com/engine/install/
+
+And also for direct download:
+https://github.com/docker/compose
+
+──────────────────────────────────────────────────────'
+
 set -- docker compose "$@"
 
 exec "$@"
